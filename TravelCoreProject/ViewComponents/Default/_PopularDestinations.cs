81edd2ddc110522256fad18b02e812174f2119a5
sefaapinar <sefanur.pinar@gmail.com>
2022-05-10 22:00:35 +0300

    Desing Patterns

---
+++ TravelCoreProject/ViewComponents/Default/_PopularDestinations.cs
@@ -0,0 +1,21 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelCoreProject.ViewComponents.Default
+{
+    public class _PopularDestinations:ViewComponent
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = destinationManager.TGetList();
+
+            return View(values);
+        }
+    }
+}
