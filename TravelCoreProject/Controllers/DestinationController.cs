3d31692cef5fd0eb3c9e13031e9c452d7a56d66c
sefaapinar <sefanur.pinar@gmail.com>
2022-05-22 20:45:30 +0300

    New Theme and New Design structure

---
+++ TravelCoreProject/Controllers/DestinationController.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelCoreProject.Controllers
+{
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IActionResult Index()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+    }
+}
