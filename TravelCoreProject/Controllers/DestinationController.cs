c9ed1b3c90c031ba5694eaa76a5502ca333a7a53
sefaapinar <sefanur.pinar@gmail.com>
2022-06-01 21:49:32 +0300

    New Theme Design

--- TravelCoreProject/Controllers/DestinationController.cs
+++ TravelCoreProject/Controllers/DestinationController.cs
@@ -5,16 +5,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Entity.Concrete;
 
 namespace TravelCoreProject.Controllers
 {
     public class DestinationController : Controller
     {
         DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+
         public IActionResult Index()
         {
             var values = destinationManager.TGetList();
             return View(values);
+        }
+        [HttpGet]
+
+        public IActionResult DestinationDetails(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DestinationDetails(Destination p)
+        {
+            return View();
         }
     }
 }
